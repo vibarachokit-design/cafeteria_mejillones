477cccf3199fa6fb3e9a8d46bbfe417284e9e732
vibarachokit-design <vibarachokit@gmail.com>
2026-05-14 15:15:05 -0400

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useEffect } from 'react';
 import { Toaster } from '@/components/ui/sonner';
 import { CartProvider } from '@/context/CartContext';
+import { MenuProvider } from '@/context/MenuContext';
 import Navbar from './sections/Navbar';
 import Hero from './sections/Hero';
 import Menu from './sections/Menu';
@@ -9,6 +10,7 @@ import Gallery from './sections/Gallery';
 import Contact from './sections/Contact';
 import Footer from './sections/Footer';
 import CartDrawer from './sections/CartDrawer';
+import AdminPanel from './sections/AdminPanel';
 
 function App() {
   useEffect(() => {
@@ -16,21 +18,24 @@ function App() {
   }, []);
 
   return (
-    <CartProvider>
-      <div className="min-h-screen bg-ocean-50/30">
-        <Navbar />
-        <main>
-          <Hero />
-          <Menu />
-          <About />
-          <Gallery />
-          <Contact />
-        </main>
-        <Footer />
-        <CartDrawer />
-        <Toaster position="top-center" richColors />
-      </div>
-    </CartProvider>
+    <MenuProvider>
+      <CartProvider>
+        <div className="min-h-screen bg-ocean-50/30">
+          <Navbar />
+          <main>
+            <Hero />
+            <Menu />
+            <About />
+            <Gallery />
+            <Contact />
+          </main>
+          <Footer />
+          <CartDrawer />
+          <AdminPanel />
+          <Toaster position="top-center" richColors />
+        </div>
+      </CartProvider>
+    </MenuProvider>
   );
 }
 
